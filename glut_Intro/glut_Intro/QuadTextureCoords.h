c78ed808e9a9e7e73f8f088d3a3e6fb517f7055c
Amsh285 <58710888+Amsh285@users.noreply.github.com>
2021-06-18 17:45:12 +0200

    fixed texturecoords

--- glut_Intro/glut_Intro/QuadTextureCoords.h
+++ glut_Intro/glut_Intro/QuadTextureCoords.h
@@ -8,17 +8,17 @@ std::vector<std::pair<float, float>> quadTextureCoords = {
 	std::pair<float, float>(0.0f, 0.0f), std::pair<float, float>(1.0f, 0.0f),
 	std::pair<float, float>(1.0f, 1.0f), std::pair<float, float>(0.0f, 1.0f),
 	// right face
-	std::pair<float, float>(1.0f, 0.0f), std::pair<float, float>(1.0f, 1.0f),
-	std::pair<float, float>(0.0f, 1.0f), std::pair<float, float>(0.0f, 0.0f),
+	std::pair<float, float>(0.0f, 0.0f), std::pair<float, float>(1.0f, 0.0f), 
+	std::pair<float, float>(1.0f, 1.0f), std::pair<float, float>(0.0f, 1.0f), 
 	// top face
-	std::pair<float, float>(0.0f, 1.0f), std::pair<float, float>(0.0f, 0.0f),
-	std::pair<float, float>(1.0f, 0.0f), std::pair<float, float>(1.0f, 1.0f),
-	// back face
-	std::pair<float, float>(1.0f, 0.0f), std::pair<float, float>(1.0f, 1.0f),
-	std::pair<float, float>(0.0f, 1.0f), std::pair<float, float>(0.0f, 0.0f),
-	// left face
 	std::pair<float, float>(0.0f, 0.0f), std::pair<float, float>(1.0f, 0.0f),
 	std::pair<float, float>(1.0f, 1.0f), std::pair<float, float>(0.0f, 1.0f),
+	// back face
+	std::pair<float, float>(1.0f, 0.0f), std::pair<float, float>(0.0f, 0.0f),
+	std::pair<float, float>(0.0f, 1.0f), std::pair<float, float>(1.0f, 1.0f),
+	// left face
+	std::pair<float, float>(1.0f, 0.0f), std::pair<float, float>(0.0f, 0.0f),
+	std::pair<float, float>(0.0f, 1.0f), std::pair<float, float>(1.0f, 1.0f),
 	// bottom face
 	std::pair<float, float>(1.0f, 1.0f), std::pair<float, float>(0.0f, 1.0f),
 	std::pair<float, float>(0.0f, 0.0f), std::pair<float, float>(1.0f, 0.0f),
